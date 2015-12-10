c8c7cb86d5df0b0070804fab2f8391b03d61aa80
joel <joel@Mac-mini-de-joel.local>
2015-12-10 16:44:00 -0300

    cambios en hilos

--- java/hilosSemaforosIII/src/hilosSemaforosIII/p1.java
+++ java/hilosSemaforosIII/src/hilosSemaforosIII/p1.java
@@ -1,21 +1,31 @@
 package hilosSemaforosIII;
 import java.util.concurrent.Semaphore;
+
 public class p1 extends Thread  {
 
-	protected Semaphore oFinP1;
-	public p1(Semaphore oFinP1) {
+	protected Semaphore oFinP1, oFinP3 ;
+	public p1(Semaphore oFinP1, Semaphore oFinP3) {
 		this.oFinP1 = oFinP1;
+		this.oFinP3 = oFinP3;
 	}
 	
 	public void run()  {
 		try {
+			this.oFinP1.acquire();//Si el contador es cero el thread se duerme, de lo contrario se reduce y obtiene el acceso 
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		/*try {
 			sleep((int) Math.round(500 * Math.random() - 0.5));
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
+		 System.out.println("*******************************************");
 		System.out.println("P1");
-		this.oFinP1.release(2);//Libera el semaphore e incrementa el countador  
-   
+		//this.oFinP1.release(2);//Libera el semaphore e incrementa el countador  
+		this.oFinP3.release();
 	}
 }
--- java/hilosSemaforosIII/src/hilosSemaforosIII/p4.java
+++ java/hilosSemaforosIII/src/hilosSemaforosIII/p4.java
@@ -3,26 +3,27 @@ import java.util.concurrent.Semaphore;
 public class p4 extends Thread  {
 
 	protected Semaphore oFinP1;
-	protected Semaphore oFinP3;
-	public p4(Semaphore oFinP1,Semaphore oFinP3) {
-		this.oFinP3 = oFinP3;
+	protected Semaphore oFinP4;
+	
+	public p4(Semaphore oFinP1,Semaphore oFinP4) {
 		this.oFinP1 = oFinP1;
+		this.oFinP4 = oFinP4;
 	}
 	
 	
 	public void run()  {
 		try {
-			this.oFinP1.acquire();//Si el contador es cero el thread se duerme, de lo contrario se reduce y obtiene el acceso 
-			this.oFinP3.acquire();
+			this.oFinP4.acquire();//Si el contador es cero el thread se duerme, de lo contrario se reduce y obtiene el acceso 
 			} catch(Exception e) {
 				e.printStackTrace();
 			}
-		try {
-			sleep((int) Math.round(500 * Math.random() - 0.5));
-		} catch (InterruptedException e) {
+		//try {
+			//sleep((int) Math.round(500 * Math.random() - 0.5));
+		//} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			//e.printStackTrace();
+		//}
 		System.out.println("P4");
+		this.oFinP1.release();
 	}
 }
